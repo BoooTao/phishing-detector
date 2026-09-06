ff6d436a5a0e1a74aea332d70d0c394ea6ffc030
BoooTao <ren.boootao@gmail.com>
2026-09-06 17:30:29 -0400

    math for entropy/randomness

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from urllib.parse import urlparse
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
+import math
+from collections import Counter
 
 FEATURE_COLS = [
     "url_length", "domain_length", "num_dots", "num_hyphens", "num_digits",
