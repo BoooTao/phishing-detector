2fb26582d6fcdc8e4530afed92cc7b5bceab95aa
BoooTao <ren.boootao@gmail.com>
2026-09-07 18:48:13 -0400

    whatever

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ import requests
 import joblib
 
 
-
-
 #feature list for model
 FEATURE_COLS = [
     "url_length", "domain_length", "num_dots", "num_hyphens", "num_digits",
@@ -191,8 +189,8 @@ def main():
     print(f"live OpenPhish recall: {caught}/{len(live_predictions)} = {caught / len(live_predictions):.3f}")
    # end of check
 
-    #joblib.dump(model, "models/phishing_rf_model.joblib", compress=3)
-    #print("random forest model exported successfully")
+    joblib.dump(model, "models/phishing_rf_model.joblib", compress=3)
+    print("random forest model exported successfully")
 
     print("fin")
 
