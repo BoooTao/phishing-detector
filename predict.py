2ba4c3f0bb2198fdb95224bfa00966731851cd25
BoooTao <ren.boootao@gmail.com>
2026-09-07 21:43:49 -0400

    removed debug

--- predict.py
+++ predict.py
@@ -21,9 +21,6 @@ def main():
 
     prediction, confidence = predict(url, model)
     print(f"{url}\n -> {prediction} ({confidence * 100:.1f}% confidence)")
-    print(extract("youtube.com"))
-    print(extract("https://youtube.com"))
-
 
 
 if __name__ == "__main__":
